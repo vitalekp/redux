206e8a3a053a196448afcacb8cfc1fa6626c9c9b
vitalekp <vitalekpushkarov@gmail.com>
2022-07-14 17:25:16 +0300

    redux config

--- lesson1-context/task1/src/App.jsx
+++ lesson1-context/task1/src/App.jsx
@@ -7,7 +7,7 @@ import { themes, ThemeContext } from './themes-context';
 // 2. divide into components +++
 // 3. static react version (верстка) ++++
 // 4. declare state (what, where) and props
-//5. write logic
+// 5. write logic
 
 class App extends Component {
   state = {
@@ -21,6 +21,7 @@ class App extends Component {
       theme: newTheme,
     });
   };
+
   render() {
     return (
       <div>
