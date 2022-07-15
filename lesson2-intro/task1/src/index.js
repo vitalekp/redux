902b0923f6f923ab4bae65cba4766be6f46e5e5a
vitalekp <vitalekpushkarov@gmail.com>
2022-07-15 12:41:58 +0300

    redux store intro

--- lesson2-intro/task1/src/index.js
+++ lesson2-intro/task1/src/index.js
@@ -3,16 +3,13 @@ import { createStore } from 'redux';
 const INCREMENT = 'COUNTER/INCREMENT';
 const DECREMENT = 'COUNTER/DECREMENT';
 
-const increment = () => {
-  return {
-    type: INCREMENT,
-  };
-};
-const decrement = () => {
-  return {
-    type: DECREMENT,
-  };
-};
+export const increment = () => ({
+  type: INCREMENT,
+});
+
+export const decrement = () => ({
+  type: DECREMENT,
+});
 
 const counterReducer = (state = 0, action) => {
   switch (action.type) {
@@ -25,7 +22,7 @@ const counterReducer = (state = 0, action) => {
   }
 };
 
-const store = createStore(counterReducer);
+export const store = createStore(counterReducer);
 
 // store.dispatch(decrement());
 // store.dispatch(increment());
