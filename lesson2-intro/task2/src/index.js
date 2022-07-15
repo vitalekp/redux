e0ecdabf2374cac8694dea7e3131f9855699e68c
vitalekp <vitalekpushkarov@gmail.com>
2022-07-15 14:07:24 +0300

    counter + history - redux

---
+++ lesson2-intro/task2/src/index.js
@@ -0,0 +1,37 @@
+import store, { increment, decrement, reset } from './store';
+import './index.scss';
+
+const resultElem = document.querySelector('.counter__result');
+const incrementBtn = document.querySelector('[data-action="increment"]');
+const resetBtn = document.querySelector('[data-action="reset"]');
+const decrementBtn = document.querySelector('[data-action="decrement"]');
+
+const onIncrement = () => {
+  store.dispatch(increment());
+};
+
+const onDecrement = () => {
+  store.dispatch(decrement());
+};
+
+const onReset = () => {
+  store.dispatch(reset());
+};
+
+incrementBtn.addEventListener('click', onIncrement);
+decrementBtn.addEventListener('click', onDecrement);
+resetBtn.addEventListener('click', onReset);
+
+store.subscribe(() => {
+  const state = store.getState();
+  const currentValue = state.history.reduce((acc, val) => acc + val, 0);
+  const historyString = state.history
+    .map(num => {
+      if (num > 0) {
+        return `+${num}`;
+      }
+      return num;
+    })
+    .join(' ');
+  resultElem.textContent = state.history.length === 0 ? '' : `${historyString} = ${currentValue}`;
+});
---
+++ lesson2-intro/task2/src/store.js
@@ -0,0 +1,45 @@
+import { createStore } from 'redux';
+
+const INCREMENT = 'COUNTER/INCREMENT';
+const DECREMENT = 'COUNTER/DECREMENT';
+const RESET = 'COUNTER/RESET';
+
+export const increment = () => ({
+  type: INCREMENT,
+});
+
+export const decrement = () => ({
+  type: DECREMENT,
+});
+
+export const reset = () => ({
+  type: RESET,
+});
+
+const initialState = [];
+
+const counterReducer = (state = { history: initialState }, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return {
+        ...state,
+        history: state.history.concat(1),
+      };
+    case DECREMENT:
+      return {
+        ...state,
+        history: state.history.concat(-1),
+      };
+    case RESET:
+      return {
+        ...state,
+        history: [],
+      };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(counterReducer);
+
+export default store;
